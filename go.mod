462da6f26b84a75b086814eac34345140c670e16
Ash Keel <ash@nebula.cafe>
2021-11-19 23:23:51 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/ashkeel/miragemod
+
+go 1.17
+
+require (
+	github.com/json-iterator/go v1.1.12
+	github.com/mattn/go-colorable v0.0.0-20170327083344-ded68f7a9561
+	github.com/nicklaw5/helix v1.25.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/strimertul/kilovolt-client-go/v2 v2.0.0
+)
+
+require (
+	github.com/DataDog/zstd v1.4.1 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
+	github.com/dgraph-io/ristretto v0.0.4-0.20210122082011-bb5d392ed82d // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/flatbuffers v1.12.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/orcaman/concurrent-map v0.0.0-20210501183033-44dafcb38ecc // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/strimertul/kilovolt/v4 v4.0.1 // indirect
+	go.opencensus.io v0.22.5 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+)
---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/mattn/go-colorable"
+	"github.com/nicklaw5/helix"
+	"github.com/sirupsen/logrus"
+
+	kvclient "github.com/strimertul/kilovolt-client-go/v2"
+)
+
+func check(err error) {
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+type FigmentEntry struct {
+	Name       string    `json:"name"`
+	Count      int       `json:"count"`
+	Total      int       `json:"total"`
+	LastRedeem time.Time `json:"last_redeem"`
+}
+
+type eventSubNotification struct {
+	Subscription helix.EventSubSubscription `json:"subscription"`
+	Challenge    string                     `json:"challenge"`
+	Event        json.RawMessage            `json:"event"`
+}
+
+func main() {
+	endpoint := flag.String("endpoint", "http://localhost:4337/ws", "Address:port to connect to")
+	auth := flag.String("auth", "", "Optional Authorization string")
+	prefix := flag.String("prefix", "mirage/", "Prefix/Namespace for keys")
+	rewardID := flag.String("reward", "a715bd7d-9454-4ff4-b91f-f74ffc97d63f", "Reward ID to check for")
+
+	log := logrus.New()
+	_ = prefix
+
+	// Ok this is dumb but listen, I like colors.
+	if runtime.GOOS == "windows" {
+		log.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+		log.SetOutput(colorable.NewColorableStdout())
+	}
+
+	headers := http.Header{}
+	if *auth != "" {
+		headers.Add("Authorization", "Bearer "+*auth)
+	}
+
+	client, err := kvclient.NewClient(*endpoint, kvclient.ClientOptions{Headers: headers})
+	check(err)
+
+	log.WithField("endpoint", *endpoint).Info("Connected to Kilovolt")
+
+	// Get chat messages
+	chat, err := client.SubscribeKey("twitch/ev/chat-message")
+	check(err)
+
+	// Get redeems
+	webhook, err := client.SubscribeKey("stulbe/ev/webhook")
+	check(err)
+
+	// Get figment counts
+	path := fmt.Sprintf("%sfigments", *prefix)
+	var figmentCounts map[string]FigmentEntry
+	err = client.GetJSON(path, &figmentCounts)
+	if err != nil {
+		if err == kvclient.ErrEmptyKey {
+			figmentCounts = make(map[string]FigmentEntry)
+			log.Info("No figment map found, creating new one")
+			_ = client.SetJSON(path, &figmentCounts)
+		} else {
+			check(err)
+		}
+	}
+
+	// Subscribe to figment count changes
+	figments, err := client.SubscribeKey(path)
+
+	for {
+		select {
+		case msg := <-figments:
+			log.Info("Figment count changed outside miragemod, updating local copy")
+			// Update figment count
+			err = jsoniter.ConfigFastest.Unmarshal([]byte(msg.Value), &figmentCounts)
+			if err != nil {
+				log.WithError(err).Error("Failed to decode new figment count, resetting")
+				figmentCounts = make(map[string]FigmentEntry)
+				continue
+			}
+		case msg := <-chat:
+			// Do nothing
+			_ = msg
+		case msg := <-webhook:
+			var payload eventSubNotification
+			err = jsoniter.ConfigFastest.Unmarshal([]byte(msg.Value), &payload)
+			if err != nil {
+				log.WithError(err).Error("Failed to decode webhook payload")
+				continue
+			}
+			if payload.Subscription.Type == "channel.channel_points_custom_reward_redemption.add" {
+				var data helix.EventSubChannelPointsCustomRewardRedemptionEvent
+				err = jsoniter.ConfigFastest.Unmarshal(payload.Event, &data)
+				if err != nil {
+					continue
+				}
+				if data.Reward.ID == *rewardID {
+					total := 0
+					count := 0
+					if entry, ok := figmentCounts[data.UserID]; ok {
+						total = entry.Total
+						count = entry.Count
+						// Check cooldown
+						if time.Since(entry.LastRedeem) < time.Hour*15 {
+							_ = say(client, "%s: You can only claim a figment once a day", data.UserName)
+							continue
+						}
+					}
+					total += 1
+					count += 1
+					figmentCounts[data.UserID] = FigmentEntry{
+						Name:       data.UserName,
+						Count:      count,
+						Total:      total,
+						LastRedeem: time.Now(),
+					}
+					err = client.SetJSON(path, &figmentCounts)
+					if err != nil {
+						log.WithError(err).Error("Failed to update figment map")
+					}
+					suffix := "th"
+					if total < 10 || total > 20 {
+						switch total % 10 {
+						case 1:
+							suffix = "st"
+						case 2:
+							suffix = "nd"
+						case 3:
+							suffix = "rd"
+						}
+					}
+					err = say(client, "%s: You claimed your ⭐ %d%s figment! ⭐ (balance: %d)", data.UserName, total, suffix, count)
+					log.WithField("user", data.UserID).WithField("reward", data.Reward.Title).Info("Redeemed")
+				}
+			}
+		}
+	}
+}
+
+func say(client *kvclient.Client, format string, args ...interface{}) error {
+	return client.SetKey("twitch/@send-chat-message", fmt.Sprintf(format, args...))
+}
